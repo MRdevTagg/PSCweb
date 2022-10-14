fe44eeabb979ded5a60a2191d1e4706ef0b48a45
MRdevTagg <93151848+MRdevTagg@users.noreply.github.com>
2022-10-14 01:51:02 -0300

    Delete psc.com directory

--- psc.com/shortcuts.js
+++
@@ -1,9 +0,0 @@
-const $ = selector => document.querySelector(selector);
-
-const $$ = selector => document.querySelectorAll(selector) ;
-const $ch = (parent, child) => document.querySelector(parent).querySelector(child);
-const EV = (selector, evnt, callback) =>  selector.addEventListener(evnt, callback);
-const isClass = (sel, classname) => sel.classList.contains(classname);
-const addClass = (selector, classname) => selector.classList.add(classname);
-const removeClass = (selector, classname) => selector.classList.remove(classname);
-const rect = sel => sel.getBoundingClientRect();
