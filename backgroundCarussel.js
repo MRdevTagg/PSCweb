4d680a45f6f707c31295e5c4959029086e1f49ae
MRdevTagg <93151848+MRdevTagg@users.noreply.github.com>
2022-11-06 17:18:25 -0300

    diseño

--- backgroundCarussel.js
+++ backgroundCarussel.js
@@ -4,6 +4,12 @@ const bb_equipo =
 'Img/Recursos/c1.jpeg',
 'Img/Recursos/c1.jpeg',
 'Img/Recursos/c1.jpeg',];
+let counter = 0;
+let index = 0;
+
+
+
+
 
 const bgChange = ({
 	src = ['Img/Recursos/c1.jpeg',
@@ -11,30 +17,23 @@ const bgChange = ({
 	'Img/Recursos/c3.jpeg',
 	'Img/Recursos/c4.jpeg',
 	'Img/Recursos/c5.jpeg',],
-	index = 0,
-	time = 7000,
+	time_seconds = 7,
 	bg1 = 'bg1',
-	bg2 = 'bg2',
-	notFirstTime = false
+	bg2 = 'bg2',	
 }) =>{
-	ID(bg1).style.background = `url(${src[index]})`;
-	ID(bg1).style.opacity = '1';
+	counter +=1
 
-	
-	EV(ID(bg1),'transitionend',()=>{
-			ID(bg2).style.opacity = '0';
-	})
-
-	index < src.length -1 ?
-	index +=1 :
-	index = 0;
-	setInterval(()=>{
-		if(notFirstTime){
+	if(counter === time_seconds*60){
+		counter = 0
+		index < src.length -1 ?
+		index +=1 :
+		index = 0;
+		
 			let temp1 = bg1;
-		let	temp2 = bg2;
+			let	temp2 = bg2;
 			bg2 = temp1;
 			bg1 = temp2;
-		}
+
 		ID(bg1).style.background = `url(${src[index]})`;
 		ID(bg1).style.opacity = '1';
 
@@ -43,10 +42,10 @@ const bgChange = ({
 				ID(bg2).style.opacity = '0';
 		})
 
-		index < src.length -1 ?
-		index +=1 :
-		index = 0;
-		if (!notFirstTime) { notFirstTime = true; }
-	},time)
+
+		
+	}
+	window.requestAnimationFrame(bgChange)
 }
-EV(window,'load',bgChange)
+
+window.requestAnimationFrame(bgChange)
